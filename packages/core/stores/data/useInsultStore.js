8341314a8d3008d5afee9d6289abe28f179c076d
StefanMogren <stefan_mogren@hotmail.com>
2025-04-24 13:32:19 +0200

    Added removeInsult to useInsultStore

--- packages/core/stores/data/useInsultStore.js
+++ packages/core/stores/data/useInsultStore.js
@@ -1,13 +1,15 @@
 import { create } from 'zustand';
-
+// https://santosnr6.github.io/Data/insults.json
 const insulting = [
 	{
 		insult: 'Bad',
 		play: 'some person',
+		id: 1,
 	},
 	{
 		insult: 'Mean',
 		play: 'someone',
+		id: 2,
 	},
 ];
 
@@ -23,9 +25,15 @@ export const useInsultStore = create((set) => ({
 				{
 					insult: newInsult,
 					play: fromPlay,
-					// id:
+					id: id,
 				},
 			],
 		}));
+	},
+	removeInsult: (id) => {
+		set((state) => {
+			const newState = state.insults.filter((insult) => insult.id !== id);
+			return newState;
+		});
 	},
 }));
