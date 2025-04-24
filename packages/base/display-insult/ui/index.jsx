5d78823b8f55fab90afdc40e1d886733b0539850
StefanMogren <stefan_mogren@hotmail.com>
2025-04-24 14:29:08 +0200

    Added CSS to ShowRandomPage, added CSS to DisplayInsult

--- packages/base/display-insult/ui/index.jsx
+++ packages/base/display-insult/ui/index.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
 import './index.css';
-export function DisplayInsult({ insult, play, id }) {
+export function DisplayInsult({ insult, play }) {
 	return (
-		<li className='insult-item' key={id}>
+		<>
 			<p className='insult-item__insult'>{insult}</p>
 			<p className='insult-item__play'>{play}</p>
-		</li>
+		</>
 	);
 }
--- packages/pages/show-random-page/ui/index.jsx
+++ packages/pages/show-random-page/ui/index.jsx
@@ -1,3 +1,15 @@
+import { DisplayInsult } from '@myApp/display-insult';
+import './index.css';
 export function ShowRandomPage() {
-	return <section className='page'>ShowRandomPage</section>;
+	return (
+		<section className='random-page'>
+			<h1 className='random-page__title'>Random insult</h1>
+			<section className='random-page__insult'>
+				<DisplayInsult
+					insult={'you are a meanie'}
+					play={'"some place"'}
+				/>
+			</section>
+		</section>
+	);
 }
